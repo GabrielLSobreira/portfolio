c1d7daa9adafeee85dae9db285197e504ffc475b
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-26 15:00:09 -0300

    feat: adição das tecnologias

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiJavascript, DiReact, DiFirebase } from 'react-icons/di';
 import {
   Section,
   SectionDivider,
@@ -14,6 +14,57 @@ import {
   ListTitle,
 } from './TechnologiesStyles';
 
-const Technologies = () => <div>Tecnologias</div>;
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Tecnologias</SectionTitle>
+    <SectionText>
+      Estou estudando diversas tecnologias e realizando cursos como B7Web,
+      ProgramadorBR e Origamid. Diariamente desenvolvo projetos para aplicar na
+      prática os conhecimentos adquiridos.
+    </SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <DiJavascript size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experiência com <br />
+            HTML, CSS, Javascript e <br />
+            Typescript
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiReact size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Frameworks</ListTitle>
+          <ListParagraph>
+            Experiência com <br />
+            React, NextJS, Bootstrap e
+            <br /> Tailwind CSS.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experiência com <br />
+            Node.js, MongoDB e SQL
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
+);
 
 export default Technologies;
