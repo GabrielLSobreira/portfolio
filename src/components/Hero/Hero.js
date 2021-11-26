d8c93a65768d60eca27aaa42f5a3058a0fb18626
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-26 12:03:12 -0300

    feat: adição das informações e estilos ao hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Gabriel Sobreira <br />
+          Desenvolvedor Web
+        </SectionTitle>
+        <SectionText>
+          Desenvolvedor focado em Front-end com conhecimento e prática em
+          JavaScript e ecossistema React.
+        </SectionText>
+        <Button
+          onClick={() =>
+            (window.location = 'mailto:gabriellaranjasobreira@gmail.com')
+          }
+        >
+          Entre em contato
+        </Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
