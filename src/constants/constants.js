8c87d0e54f85da1cdbb0ed16b6faef3416afd2bb
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-26 14:44:00 -0300

    feat: informações e imagens dos projetos

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,57 @@
-export const projects = [];
+export const projects = [
+  {
+    title: 'Next Games',
+    description:
+      'O site é um mini e-commerce de jogos, o projeto foi desenvolvido com NextJS e utilizei o Prismic como sistema de gerenciamento de conteúdo.',
+    image: '/images/1.png',
+    tags: ['NextJS', 'TypeScript', 'Prismic'],
+    visit: ' https://gsnextgames.vercel.app/',
+    id: 0,
+  },
+
+  {
+    title: 'Dogs',
+    description:
+      'O site é uma rede Social de cachorro, onde é possível postar fotos, fazer comentários, cadastro, login e verificar estatísticas.',
+    image: '/images/2.png',
+    tags: ['React', 'JavaScript', 'CSS'],
+    visit: 'https://gsdogs.netlify.app/',
+    id: 1,
+  },
+  {
+    title: 'MoviesApp',
+    description:
+      'Site de filmes, onde é possível ver os detalhes de um filme, fazer busca, selecionar por categoria entre outros.',
+    image: '/images/3.png',
+    tags: ['React', 'TypeScript'],
+    visit: 'https://gsdogs.netlify.app/',
+    id: 2,
+  },
+  {
+    title: 'Chamados',
+    description:
+      'Sistema de chamados, onde é possível fazer login, registrar clientes, trocar imagem de avatar, entre outros.',
+    image: '/images/4.png',
+    tags: ['React', 'TypeScript', 'CSS'],
+    visit: 'https://gsdogs.netlify.app/',
+    id: 3,
+  },
+  {
+    title: 'Netflix Clone',
+    description:
+      'Clone da página principal da Netflix, foi desenvolvido com ReactJS.',
+    image: '/images/5.png',
+    tags: ['React', 'JavaScript', 'CSS'],
+    visit: 'https://gsdogs.netlify.app/',
+    id: 3,
+  },
+  {
+    title: 'Hulu Clone',
+    description:
+      'Clone da página principal da Hulu, foi desenvolvido com NextJS e Taiwind CSS.',
+    image: '/images/6.png',
+    tags: ['NextJS', 'Taiwind CSS'],
+    visit: 'https://gsdogs.netlify.app/',
+    id: 3,
+  },
+];
