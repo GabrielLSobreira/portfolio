fafb16e4daf474728a78269504b4f954353805e9
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-26 15:21:39 -0300

    feat: adição do sobre

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,22 @@
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
+
 const About = () => {
-  return <div>Sobre</div>;
+  return (
+    <Section id="about">
+      <SectionTitle>Sobre mim</SectionTitle>
+      <SectionText>
+        Tenho 23 anos e estou cursando Análise e Desenvolvimento de Sistemas,
+        meu maior interesse é no desenvolvimento Front-end. Já ajudei em alguns
+        Freelancers e estou em busca de uma oportunidade como Desenvolvedor
+        Júnior ou Estagiário.
+      </SectionText>
+    </Section>
+  );
 };
 
 export default About;
