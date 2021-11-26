e035ea94adf5ac6bbfbc26cd50e52c6518107580
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-26 16:13:58 -0300

    fix: bug de não aparecer a imagem no deploy corrigido

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'Next Games',
     description:
       'O site é um mini e-commerce de jogos, o projeto foi desenvolvido com NextJS e utilizei o Prismic como sistema de gerenciamento de conteúdo.',
-    image: '/images/1.png',
+    image: '/images/1.PNG',
     tags: ['NextJS', 'TypeScript', 'Prismic'],
     visit: ' https://gsnextgames.vercel.app/',
     id: 0,
@@ -13,7 +13,7 @@ export const projects = [
     title: 'Dogs',
     description:
       'O site é uma rede Social de cachorro, onde é possível postar fotos, fazer comentários, cadastro, login e verificar estatísticas.',
-    image: '/images/2.png',
+    image: '/images/2.PNG',
     tags: ['React', 'JavaScript', 'CSS'],
     visit: 'https://gsdogs.netlify.app/',
     id: 1,
@@ -22,7 +22,7 @@ export const projects = [
     title: 'MoviesApp',
     description:
       'Site de filmes, onde é possível ver os detalhes de um filme, fazer busca, selecionar por categoria entre outros.',
-    image: '/images/3.png',
+    image: '/images/3.PNG',
     tags: ['React', 'TypeScript'],
     visit: 'https://gsmoviesapp.netlify.app/',
     id: 2,
@@ -31,7 +31,7 @@ export const projects = [
     title: 'Chamados',
     description:
       'Sistema de chamados, onde é possível fazer login, registrar clientes, trocar imagem de avatar, entre outros.',
-    image: '/images/4.png',
+    image: '/images/4.PNG',
     tags: ['React', 'Javascript', 'Firebase'],
     visit: 'https://gschamados.netlify.app/dashboard',
     id: 3,
@@ -40,7 +40,7 @@ export const projects = [
     title: 'Netflix Clone',
     description:
       'Clone da página principal da Netflix, foi desenvolvido com ReactJS.',
-    image: '/images/5.png',
+    image: '/images/5.PNG',
     tags: ['React', 'JavaScript', 'CSS'],
     visit: 'https://gsnetflix.netlify.app/',
     id: 4,
@@ -49,7 +49,7 @@ export const projects = [
     title: 'Hulu Clone',
     description:
       'Clone da página principal da Hulu, foi desenvolvido com NextJS e Taiwind CSS.',
-    image: '/images/6.png',
+    image: '/images/6.PNG',
     tags: ['NextJS', 'Taiwind CSS'],
     visit: 'https://gshulu-clone.vercel.app/',
     id: 5,
